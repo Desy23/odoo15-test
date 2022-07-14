f2e721040e0ba5a8a87360ba11a9282b804d2387
Destiny <92610618+Desy23@users.noreply.github.com>
2022-07-14 13:37:50 -0400

    Add files via upload

---
+++ custom_addons/travel_management/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Travel Management",
+    'category': 'Extra tools',
+    'version': '1.1',
+    'depends': ['base', 'mail', 'uom'],
+    'description': """
+    """,
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
