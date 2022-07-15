4f5ef7bf7ea024f2fd347e7923a8cd4185161e0b
Destiny <92610618+Desy23@users.noreply.github.com>
2022-07-15 10:58:55 -0400

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hello-world",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': "Travel Management",
+    'category': 'Extra tools',
+    'version': '1.1',
+    'depends': ['base', 'base_setup', 'mail', 'uom'],
     'description': """
-        Long description of module's purpose
     """,
-
-    'author': "dwilliams",
-    'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "security/ir.model.access.csv",
     ],
-    # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
